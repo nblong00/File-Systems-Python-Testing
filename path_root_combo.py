d71bbd5e64aa1e97c0fd86a56d6109d7523b6474
Blaise <blazzze0517@gmail.com>
2025-07-20 11:05:40 -0400

    Cleaned up program and added prints/inputs for info providing.

--- path_root_combo.py
+++ path_root_combo.py
@@ -6,7 +6,10 @@ root =  input("Provie root: ")
 def absolute(path, root):
     if not os.path.isabs(path):
         fixed_path = os.path.join(root, path)
+        print("This is fixed!")
         return fixed_path
+    print("The path is already absolute...")
     return path
 
 print(absolute(path, root))
+input("Press ENTER to close program...")
