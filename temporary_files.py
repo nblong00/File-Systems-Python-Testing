c6c30c7b5da85f24d87e6a42f8e785046c5ffd86
Blaise <blazzze0517@gmail.com>
2025-07-29 13:50:48 -0400

    Organized code to live in functions. Also added if __name__ statement.

--- temporary_files.py
+++ temporary_files.py
@@ -1,8 +1,16 @@
 import os
 import tempfile
 
-with tempfile.TemporaryDirectory() as tempdirname:
-    print(f"Created temporary directory named {tempdirname}")
-    with open(os.path.join(tempdirname, 'temporary_file.txt'), 'w') as f:
-        f.write("This is a temporary file...")
-    input()
+
+def write_temp_file():
+    with tempfile.TemporaryDirectory() as tempdirname:
+        print(f"Created temporary directory named {tempdirname}")
+        with open(os.path.join(tempdirname, "temporary_file.txt"), "w") as f:
+            f.write("This is a temporary file...")
+        input()
+
+def main():
+    write_temp_file()
+
+if __name__ == "__main__":
+    main()
